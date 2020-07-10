203275eece172576ea95ea66f51f1972c89c9b96
achlanbima <achlan_00011101@hotmail.com>
2020-07-10 09:27:40 +0700

    navigator hookup

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SignupScreen from './src/screens/SignupScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import AccountScreen from './src/screens/AccountScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
 });
+
+export default createAppContainer(switchNavigator);
