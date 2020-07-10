00cf2ba8d074df7e170ac78be7eb343969db9ea1
achlanbima <achlan_00011101@hotmail.com>
2020-07-10 09:15:08 +0700

    init screens

---
+++ src/screens/TrackDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TrackDetailScreen = () => {
+    return (
+        <View>
+            <Text>Track Detail</Text>
+        </View>
+    )
+}
+
+export default TrackDetailScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TrackListScreen = () => {
+    return (
+        <View>
+            <Text>TrackList Screen</Text>
+        </View>
+    )
+}
+
+export default TrackListScreen
+
+const styles = StyleSheet.create({})
