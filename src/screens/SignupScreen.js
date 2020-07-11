14dae3b9a695a1f06061131a955c544c00c888f7
achlanbima <achlan_00011101@hotmail.com>
2020-07-11 20:14:59 +0700

    add input props & fix keyboard avoiding view

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,43 @@
-import React from 'react'
-import { StyleSheet, View} from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View, KeyboardAvoidingView} from 'react-native';
 import { Input, Text, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input label="Email" />
-            <Spacer/>
-            <Input label="Password" />
-            <Spacer/>
-            <Spacer>
-                <Button title="Sign Up" />
-            </Spacer>
-        </View>
+        <KeyboardAvoidingView>
+            <ScrollView contentContainerStyle={styles.containerScroll}>
+                <View style={styles.container}>
+                    <Spacer>
+                        <Text h3>Sign Up for Tracker</Text>
+                    </Spacer>
+                    <Input 
+                        label="Email" 
+                        value={email} 
+                        onChangeText={(value)=>setEmail(value)} 
+                        autoCapitalize={"none"}
+                        autoCorrect={false}    
+                    />
+                    <Spacer/>
+                    <Input 
+                        label="Password" 
+                        value={password} 
+                        onChangeText={(value)=>setPassword(value)} 
+                        autoCapitalize={"none"}
+                        autoCorrect={false} 
+                        secureTextEntry={true}
+                    />
+                    <Spacer/>
+                    <Spacer>
+                        <Button title="Sign Up" />
+                    </Spacer>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -32,6 +53,9 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent:'center',
-        marginBottom:250
+        marginBottom:250,
+    },
+    containerScroll:{
+        paddingVertical:100
     }
 })
