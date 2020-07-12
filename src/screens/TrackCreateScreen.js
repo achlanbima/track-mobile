5be80559c55f4834f81fba67b9ccd6e538a0caad
achlanbima <achlan_00011101@hotmail.com>
2020-07-13 06:54:46 +0700

    set permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import Map from '../components/Map'
 import { Text } from 'react-native-elements'
+import { requestPermissionsAsync } from 'expo-location'
 
 const TrackCreateScreen = () => {
+
+    const [err, setErr] = useState('');
+
+    const startWatching = async () => {
+        try {
+          const { granted } = await requestPermissionsAsync();
+          if (!granted) {
+            throw new Error('Location permission not granted');
+          }
+        } catch (e) {
+          setErr(e);
+        }
+      };
+
+    useEffect(()=>{
+        startWatching()
+    },[]);
+
+    console.log(err);
+
     return (
         <SafeAreaView forceInset={{top:'always'}} >
             <Text h3>TrackCreate Screen</Text>
             <Map/>
+            { err ? (<Text>Please grant permisson</Text>) : null }
         </SafeAreaView>
     )
 }
