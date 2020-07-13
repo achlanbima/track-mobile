2086f7fa41938604210a3cd9060bfd8799c361ab
achlanbima <achlan_00011101@hotmail.com>
2020-07-13 22:30:15 +0700

    implement to maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text } from 'react-native-elements';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, ActivityIndicator } from 'react-native';
+import MapView, { Circle } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
+    const { state: { currentLocation } } = useContext(LocationContext);
+
+    if(!currentLocation){
+        return <ActivityIndicator size="large" style={{marginTop: 200}} />
+    }
+    // console.log(currentLocation);
     return (
         <MapView
             style={styles.map}
             initialRegion={{
-                latitude: 37.33233,
-                longitude: -122.03121,
+                ...currentLocation.coords,
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
             }}
-        />
+        >
+            <Circle
+                center={currentLocation.coords}
+                radius={30}
+                strokeColor="rgba(255,0,0,1.0)"
+                fillColor="rgba(255,0,0,0.3)"
+            />
+        </MapView>
     )
 }
 
