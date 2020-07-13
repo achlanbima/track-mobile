bc6facc120c1b5a36ae7800c72e05c33df8d4c74
achlanbima <achlan_00011101@hotmail.com>
2020-07-13 22:29:50 +0700

    implement to trackcreatescreen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,14 @@
 import '../_mockLocation';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import Map from '../components/Map'
 import { Text } from 'react-native-elements'
-import { requestPermissionsAsync } from 'expo-location'
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackCreateScreen = () => {
-
+    const { addLocation } = useContext(LocationContext);
     const [err, setErr] = useState('');
 
     const startWatching = async () => {
@@ -16,6 +17,13 @@ const TrackCreateScreen = () => {
           if (!granted) {
             throw new Error('Location permission not granted');
           }
+          await watchPositionAsync({
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          }, (location)=> {
+            addLocation(location);
+          });
         } catch (e) {
           setErr(e);
         }
