cb7c9459d7bafb010ede251545bfed2cd27498e3
achlanbima <achlan_00011101@hotmail.com>
2020-07-14 20:49:56 +0700

    implement context to trackform

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import Spacer from './Spacer'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackForm = () => {
+    const {
+        state: {name, recording, locations}, 
+        startRecording, 
+        stopRecording,
+        changeName } = useContext(LocationContext);
+
+    console.log(locations.length);
+
     return (
         <>
             <Spacer>
-                <Input placeholder="Enter name" />
+                <Input 
+                    value={name}
+                    placeholder="Enter name" 
+                    onChangeText={changeName}    
+                />
             </Spacer>
-            <Button title="Start Recording" />
+            {recording 
+                ? <Button title="Stop Recording" onPress={stopRecording} /> 
+                : <Button title="Start Recording" onPress={startRecording} />
+            }
+            
         </>
     )
 }
