0b29a1fb0345130e40eef0511d58f5e26133a282
achlanbima <achlan_00011101@hotmail.com>
2020-07-15 19:52:55 +0700

    reset when submit

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -11,7 +11,9 @@ const locationReducer = (state, action) => {
         case 'add_location':
             return {...state, locations: [...state.locations, action.payload]};
         case 'change_name':
-            return {...state, name: action.payload}
+            return {...state, name: action.payload};
+        case 'reset':
+            return {...state, name: '', locations: []};
         default:
             return state;
     }
@@ -33,9 +35,12 @@ const addLocation = dispatch => (location, recording) => {
         dispatch({ type: 'add_location', payload: location});
     }
 };
+const reset = dispatch => () => {
+    dispatch({type: 'reset'});
+};
 
 export const { Context, Provider } = createDataContext(
     locationReducer,
-    {startRecording, stopRecording, addLocation, changeName},
+    {startRecording, stopRecording, addLocation, changeName, reset},
     { name:'', recording: false, locations: [], currentLocation: null }
 );
--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,13 +1,16 @@
 import { useContext } from 'react';
 import { Context as TrackContext } from '../context/TrackContext';
 import { Context as LocationContext } from '../context/LocationContext';
+import { navigate } from '../navigationRef';
 
 export default () => {
     const { createTracks } = useContext(TrackContext);
-    const { state: {locations, name} } = useContext(LocationContext);
+    const { state: {locations, name}, reset } = useContext(LocationContext);
     
-    const saveTrack = () => {
-        createTracks(name, locations);
+    const saveTrack = async () => {
+        await createTracks(name, locations);
+        reset();
+        navigate('TrackList');
     }
 
     return [saveTrack];
