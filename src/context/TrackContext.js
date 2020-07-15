d2c55cee646f48c31ec4f95ca5e5eac0bf5556d7
achlanbima <achlan_00011101@hotmail.com>
2020-07-15 19:55:31 +0700

    create fetchTracks

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -1,14 +1,20 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker'
 ;
+import { AsyncStorage } from 'react-native';
 const trackReducer = (state, action) => {
     switch(action.type){
+        case 'fetch_tracks':
+            return action.payload;
         default:
             return state;
     }
 }
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+    const response = await trackerApi.get('/tracks');
+    dispatch({ type: 'fetch_tracks', payload: response.data});
+};
 const createTracks = dispatch => async (name, locations) => {
     await trackerApi.post('/tracks', {name, locations});
 };
